7bf12e6305f97d3026756190c5fd9425bc39be20
ferrisbueller66 <tmtarpinian@gmail.com>
2020-09-21 23:00:52 -0400

    wrote sorting logic, need to debug button event

--- algorithms/src/Components/SortingArrays.js
+++ algorithms/src/Components/SortingArrays.js
@@ -1,42 +1,41 @@
 import React, { useState } from "react"
 import { NavLink } from 'react-router-dom';
 
+
+
 const SortingArray = () => {
-
-    const [array, setArray] = useState([2, 10, 54, 82, 9343, 23, 1, 34, 50, 941])
-
-    const handleOnClick = () =>{
-        setArray([...array, Math.floor(Math.random() * 125)])
+    
+    
+    const array = [2, 10, 54, 82, 9343, 23, 1, 34, 50, 941]
+  
+    const findAndRemoveMin = (array) => {
+        let min = array[0]
+        let lowIndex = 0
+        for (let i = 1; i < array.length; i++){
+            if (array[i] < min){
+                min = array[i];
+                lowIndex = i
+            }
+        }
+        array.splice(lowIndex, 1)
+        return min
+    }
+    
+    const sortedArray = (array) =>{
+        let newMin
+        let newArray = []
+            while (array.length > 0){
+        console.log(array)
+            newMin = findAndRemoveMin(array)
+                newArray.push(newMin)
+            console.log(newArray)
+        }
+        console.log(newArray)
+        return newArray
     }
 
-    const handleOnClick2 = () =>{
-        let sortedArray =[]
-        let lowNumber = parseInt(array[0])
-        let lowIndex = 0
-        while (array.length > 0){
-            for (let i = 1; i<array.length; i++){
-                if (array[i] < lowNumber){
-                    lowNumber = array[i]
-                    lowIndex = i
-                }
-            sortedArray.push(lowNumber)
-            array.splice(lowIndex+1).concat(array.slice(lowIndex, 1))
-            console.log(array)
-            }
-            console.log(sortedArray)
-            
-        }
-       //take first number, set as lowest
-       //take first index, set as lowIndex
-       //iterate and compare each number
-            //if find lower number, keep iterating
-            //when done, push number to sorted array
-            //splice out low number from old array
-            //start iteration again
-       
-        let
-       
-        array
+    const handleOnClick2 = (array) =>{
+        findAndRemoveMin(array)
     }
 
     return(
@@ -44,13 +43,13 @@ const SortingArray = () => {
             <h1>Here's an array</h1>
             {array}
             <br/>
-            <button onClick={handleOnClick}>
+            <button>
                     Click this to add a Random number to the array
             </button>
             <br/>
             <br/>
         
-            <button onClick={handleOnClick2}>
+            <button onClick={sortedArray}>
                     Click this to sort the array from lowest to greatest
             </button>
             <br/>
